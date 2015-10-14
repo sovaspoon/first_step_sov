d4e270d9f93f4ed685508696edfac5f033088045
Ksenia <kseniyakunina@gmail.com>
2015-10-15 01:27:08 +0300

    первый шаг сов

---
+++ main.js
@@ -0,0 +1,13 @@
+var list = ['owl', 'owl', 'owl', 'cat', 'dog', 'cow', 'lion', 'cow'];
+var item;
+var hash = {};
+
+list.forEach(function (item) {
+    if (hash.hasOwnProperty(item)) {
+        ++hash[item]
+    } else {
+        hash[item] = 1;
+    }
+});
+
+console.log(hash);
