8cec942b1254ca892202a5adaf8b6b7b9a262f1f
Ksenia <kseniyakunina@gmail.com>
2015-10-15 01:41:47 +0300

    кратко

--- main.js
+++ main.js
@@ -2,11 +2,7 @@ var list = ['owl', 'owl', 'owl', 'cat', 'dog', 'cow', 'lion', 'cow'];
 var hash = {};
 
 list.forEach(function (item) {
-    if (hash.hasOwnProperty(item)) {
-        ++hash[item]
-    } else {
-        hash[item] = 1;
-    }
+    hash[item] = 1 + ~~hash[item];
 });
 
 console.log(hash);
